23fb793460e7c1931038508aff32a1f59cadf41a
KWONJYE <sheisdanmi@gmail.com>
2025-01-13 19:48:18 +0900

    [2/예제 만들기] 로그 추적기 V1 - 프로토타입 개발

---
+++ advanced/src/main/java/hello/advanced/trace/TraceId.java
@@ -0,0 +1,35 @@
+package hello.advanced.trace;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class TraceId {
+
+    private String id;
+    private int level;
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    private TraceId createNextId() {
+        return new TraceId(id, level + 1);
+    }
+
+    public TraceId createPrevId() {
+        return new TraceId(id, level - 1);
+    }
+
+    public boolean isFirstLevel() {
+        return level == 0;
+    }
+}
