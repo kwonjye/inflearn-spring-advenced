fa77103923bc1797b200be42206a980ea8f4c89b
KWONJYE <sheisdanmi@gmail.com>
2025-02-04 19:36:15 +0900

    [5/프록시 패턴과 데코레이터 패턴] 예제 프로젝트 만들기 v3

--- proxy/src/main/java/hello/proxy/app/v2/OrderControllerV2.java
+++ proxy/src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -3,7 +3,6 @@ package hello.proxy.app.v2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
@@ -18,7 +17,7 @@ public class OrderControllerV2 {
     }
 
     @GetMapping("/v2/request")
-    public String request(@RequestParam("itemId") String itemId) {
+    public String request(String itemId) {
         orderService.orderItem(itemId);
         return "ok";
     }
