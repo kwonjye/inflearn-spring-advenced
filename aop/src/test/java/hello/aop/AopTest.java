a40b24433ca83e29308cddc2e8bb6d5ae9b54b68
KWONJYE <sheisdanmi@gmail.com>
2025-03-21 20:49:58 +0900

    [11/스프링 AOP 구현] 스프링 AOP 구현5 - 어드바이스 순서

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -16,7 +16,8 @@ import org.springframework.context.annotation.Import;
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
 //@Import(AspectV3.class)
-@Import(AspectV4Pointcut.class)
+//@Import(AspectV4Pointcut.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
 public class AopTest {
 
     @Autowired
